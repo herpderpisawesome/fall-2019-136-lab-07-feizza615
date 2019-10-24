9521d21d3f50bdfde5f65bb1ac514ed4391679be
Feizza Fazilatun <feizzafazilatun@Feizzas-MacBook-Air.local>
2019-10-24 10:59:18 -0400

    funcs.h

---
+++ funcs.h
@@ -0,0 +1,8 @@
+#pragma once
+
+std::string fileReader(std::string fileName);
+std::string removeLeadingSpaces(std::string line);
+//int countChar(std::string line, char c);
+std::string indent(std::string line);
+bool checkChar(std::string str, char c);
+std::string stringCreator(std::string c, int times);
