c604482cb9df8a3d9c7a18b2f8ae3b807ce260d6
Feizza Fazilatun <feizzafazilatun@Feizzas-Air.home>
2019-10-27 02:07:56 -0400

    funcs.h

--- funcs.h
+++ funcs.h
@@ -2,7 +2,6 @@
 
 std::string fileReader(std::string fileName);
 std::string removeLeadingSpaces(std::string line);
-//int countChar(std::string line, char c);
 std::string indent(std::string line);
 bool checkChar(std::string str, char c);
 std::string stringCreator(std::string c, int times);
