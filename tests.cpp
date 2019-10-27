64d2fbf4ee852499d419f44f177b9b40ea26419a
Feizza Fazilatun <feizzafazilatun@Feizzas-Air.home>
2019-10-27 02:08:43 -0400

    tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -3,3 +3,17 @@
 
 #include "doctest.h"
 #include "funcs.h"
+#include <string>
+
+
+
+
+TEST_CASE("Single line Function"){
+  CHECK(fileReader("test1.cpp").compare("int main(){\n\tcout << i;\n}\n")==0);
+}
+
+TEST_CASE("Multiple Function File"){
+  std::string str="int main(){\n\t// Hi, I'm a program!\n\tint x = 1;\n\tfor(int i = 0; i < 10; i++) {\n\t\tcout << i;\n\t\tcout << endl;\n\t}\n}\n";
+  std::cout<<str;
+  CHECK(fileReader("tests2.cpp").compare(str)==0);
+}
